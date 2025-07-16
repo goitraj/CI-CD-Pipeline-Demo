504d162207bc265d34c47ca13f8b334bf9ea8541
mukraj <mukesh.raj@publicissapient.com>
2025-07-16 17:56:14 +0530

    new commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Welcome to Docker Deployment..!!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("Hello from Java inside Git repo!");
